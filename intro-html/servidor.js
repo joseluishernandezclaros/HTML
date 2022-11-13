499ba18dfeff31727d397b35660c0083c5c07f33
joseluishernandezclaros <joseluishc1966@gmail.com>
2022-11-13 12:12:48 -0500

    first commit

---
+++ intro-html/servidor.js
@@ -0,0 +1,15 @@
+const http = require('http');
+
+function sitioweb(req, res){
+    res.writeHead(200, {"Content-Type": "text/html;"});
+    /* res.end("Mi primer servidor"); */
+    /* res.end("Mi <strong>segundo</strong> servidor"); */
+    res.end("como meto un html completo :c");
+}
+
+let servidor = http.createServer(sitioweb);
+
+servidor.listen(7070, "192.168.1.17");
+
+console.log("Servidor corriendo en el puerto 7070");
+
