3f70b4f30fb89baf31c0fcda694b11654e97815f
joseluishernandezclaros <joseluishc1966@gmail.com>
2022-11-13 22:29:05 -0500

    html en servidor node

--- intro-html/servidor.js
+++ intro-html/servidor.js
@@ -1,10 +1,54 @@
 const http = require('http');
 
+
 function sitioweb(req, res){
     res.writeHead(200, {"Content-Type": "text/html;"});
     /* res.end("Mi primer servidor"); */
     /* res.end("Mi <strong>segundo</strong> servidor"); */
-    res.end("como meto un html completo :c");
+    res.end(`
+    <!DOCTYPE html>
+<html>
+<head>
+    <title>Document</title>
+    <meta charset="UTF-8"/>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header id="titulo">
+        <h1>Blog de chanchito feliz</h1>
+    </header>
+    <section class="container">
+        <article class="post">
+            <h2>Título de mi post</h2>
+            <p>Descripción de mi post</p>
+            <img src="img/coffee.png" width="400" />
+            <p>Texto del artículo que hay acá y es texto de prueba. Chanchito feliz!</p>
+        </article>
+        <article class="post">
+            <h2>Título de mi post 2</h2>
+            <p>Descripción de mi post 2</p>
+            <img src="img/monster.png" width="400" />
+            <p>Texto del artículo que hay acá y es texto de prueba. Chanchito feliz!</p>
+        </article>
+        <article class="post">
+            <h2>Título de mi post 3</h2>
+            <p>Descripción de mi post 3</p>
+            <img src="img/guitarra.png" width="400" />
+            <p>Texto del artículo que hay acá y es texto de prueba. Chanchito feliz!</p>
+        </article>
+    </section>
+    <footer>
+        <section>
+            <a href="#titulo">Ir al comienzo</a>
+            <a href="mailto:nicolas@gmail.com">Contáctame aquí</a>    
+        </section>
+         <p>Copyright 2021</p>
+    </footer>
+</body>
+</html>
+
+
+    `);
 }
 
 let servidor = http.createServer(sitioweb);
